9cff766dbee55da1daa124a65ab0e0ad41029598
Miles Silberling-Cook <nth.tensor@gmail.com>
2022-07-18 13:39:17 -0500

    Add function doc for load_graph

--- w0_d1/load_graph.m
+++ w0_d1/load_graph.m
@@ -1,4 +1,7 @@
 function G = load_graph(N)
+  % Takes in a number of edges and generates
+  % a weighted undirected graph (by first creating
+  % a random positive-definite triangular matrix).
   t = triu(2*randn(N,N),1);
   A = max(t+t.', 0);
   G = graph(A);
