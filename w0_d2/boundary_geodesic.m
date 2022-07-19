a6e0c9db909c75635de8b71225ed87d856f5abdc
Miles Silberling-Cook <nth.tensor@gmail.com>
2022-07-19 18:08:03 -0500

    Add boundary geodesic with gradient norm

---
+++ w0_d2/boundary_geodesic.m
@@ -0,0 +1,31 @@
+% Load in mesh
+[V,F] = readOBJ('sphere.obj');
+
+% Get the number of verticies
+N = length(V);
+
+% Compute the boundary verticies
+O = outline(F);
+B = unique(O(:));
+
+% Get the number of verticies in the boundary
+NB = length(B);
+
+% Compute intenior verticies
+I = setdiff(1:N, B);
+
+% Compute the gradient
+G = grad(V, F);
+
+% Find a geodesic to a point on the boundary
+cvx_begin
+  variable f(N)
+  maximize( sum( f ) )
+  subject to
+    f(B) <= 0
+    norm(G*f) <= 1
+cvx_end
+
+tsurf(F, V, 'CData', f);
+shading interp;
+axis off;
