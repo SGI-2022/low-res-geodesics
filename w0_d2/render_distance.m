008ae5f16207ceb98f32513ccde3d326a1c70373
Miles Silberling-Cook <nth.tensor@gmail.com>
2022-07-20 07:25:28 -0500

    Add fixed axis size to render function

--- w0_d2/render_distance.m
+++ w0_d2/render_distance.m
@@ -1,8 +1,11 @@
 function render_distance(V, F, D)
+  % Renders a distance function on a surface
+
   tsurf(F, V, 'CData', D);
   shading interp;
   axis equal;
   axis off;
+  axis auto;
   shading interp;
   camproj('perspective')
 end
