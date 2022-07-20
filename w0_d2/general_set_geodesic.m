b531b78eed073f26df6aed7d31cc709e4cb67f4e
Miles Silberling-Cook <nth.tensor@gmail.com>
2022-07-20 07:27:19 -0500

    Add functiomn for general set-distance geodesic based off boundary_geodesic

---
+++ w0_d2/general_set_geodesic.m
@@ -0,0 +1,28 @@
+% Computes the geodesic distance to an arbitrary subset of verticies
+% using the gradient. Could also be implemented with linear solvers.
+
+% Load in mesh
+[V,F] = readOBJ('spot.obj');
+
+% Swap axes
+V = [V(:,1), V(:,3), V(:,2)];
+
+% Get the number of verticies
+N = length(V);
+
+% Select a subset of target verticies for set distance
+S = randi(N, 50, 1);
+
+% Compute the gradient
+G = grad(V, F);
+
+% Find a geodesic to a point on the boundary
+cvx_begin
+  variable f(N)
+  maximize( sum( f ) )
+  subject to
+    f(S) <= 0
+    norm(G*f) <= 1
+cvx_end
+
+render_distance(V, F, f)
