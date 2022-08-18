22bc43b0c7468768027b46c00ebf0e81cdc03ce1
Miles Silberling-Cook <nth.tensor@gmail.com>
2022-08-18 14:10:14 -0500

    Fix heatmap generation

--- gen_heatmap.m
+++ gen_heatmap.m
@@ -1,9 +1,9 @@
 
 NumFaceSteps = 5;
-FacesStepSize = 5;
+FacesStepSize = 10;
 
 NumBasisSteps = 5;
-BasisStepSize = 5;
+BasisStepSize = 10;
 
 % Load in mesh
 [Verts, Faces] = readOBJ('data/spot_mini.obj');
@@ -25,15 +25,20 @@ MeanError = zeros(NumFaceSteps, NumBasisSteps);
 for FaceStep = 1:NumFaceSteps
   for BasisStep = 1:NumBasisSteps
     
-    Basis = FullBasis(:, 1:BasisStep*BasisStepSize);
-    ProjectedDistance = pairwise_sparse_geodesic(Verts, Faces, Basis, 100, FaceStep * FacesStepSize);
-    AproxDistance = Basis(:, :) * ProjectedDistance * Basis(TargetVerts, :).';
+    %Basis = FullBasis(:, 1:BasisStep*BasisStepSize);
+    %ProjectedDistance = pairwise_sparse_geodesic(Verts, Faces, Basis, 130, FaceStep * FacesStepSize);
+    %AproxDistance = Basis(:, :) * ProjectedDistance * Basis(TargetVerts, :).';
+
+    Method = strcat('heatmap', string(FaceStep), '-', string(BasisStep));
+    AproxDistance = load_geodesic('spot_mini', Method);
 
     % Drop the distance at the target vert, it won't worth with relitive
     % error
     PinpointGroundTruthDistance = GroundTruthDistance(1:end ~= TargetVerts);
     PinpointAproxDistance = AproxDistance(1:end ~= TargetVerts);
-    ErrorVec = abs((PinpointGroundTruthDistance - PinpointAproxDistance) ./ PinpointGroundTruthDistance);
+    ErrorVec = abs((PinpointGroundTruthDistance - PinpointAproxDistance));
     MeanError(FaceStep, BasisStep) = sum(ErrorVec) / size(Verts, 1);
   end
 end
+
+imagesc(MeanError)
